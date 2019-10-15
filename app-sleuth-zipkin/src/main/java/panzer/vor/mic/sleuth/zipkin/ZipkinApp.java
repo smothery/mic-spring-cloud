1f1162bdf2ba2455f3ea53a644befd32ece9d756
luozhancai <944233996@qq.com>
2019-10-15 11:55:50 +0800

    zipkin

--- app-sleuth-zipkin/src/main/java/panzer/vor/mic/sleuth/zipkin/ZipkinApp.java
+++ app-sleuth-zipkin/src/main/java/panzer/vor/mic/sleuth/zipkin/ZipkinApp.java
@@ -5,6 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zipkin.server.internal.EnableZipkinServer;
 
 /**
+ * zipkin server 官方不推荐自行搭建。也可以去下载官方的
+ *
+ * 测试需启动：
+ *  app-eureka(一个或集群)
+ *
+ * 集成zipkin的服务，测试可选择启动
+ *  app-client-feign
+ *  app-product-hystrix
+ *  app-order-hystrix
+ *  app-zuul-gateway
+ *
  * Created by luozc on 2019-10-11
  */
 @SpringBootApplication
